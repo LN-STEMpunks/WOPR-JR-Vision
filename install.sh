dfdbe4c5618e2ee236c6fcb472c1e6dcce6cb92c
Cade Brown <brown.cade@gmail.com>
2017-01-23 00:14:10 -0500

    Created gitignore, readme, install

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$UID" -ne "0" ]; then
+    printf "ERROR: must be root\n"
+    sudo $0
+    exit $?
+fi
+
+apt install python-pip
+apt install libopencv-dev
+apt install python-opencv
+
+pip2 install -r requirements.txt
