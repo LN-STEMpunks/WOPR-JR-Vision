dddfca624b402bcf66176f13a816115c11a8be29
LN STEMpunks Programming <cade@cade.site>
2017-01-25 00:37:16 +0000

    Painting center

--- src-cpp/main.cpp
+++ src-cpp/main.cpp
@@ -1,17 +1,31 @@
 
 #include <cv.h>
 #include <highgui.h>
+#include "opencv2/gpu/gpu.hpp"
+
+#include <chrono>
+#include <cmath>
 
 using namespace std;
+using namespace std::chrono;
+
 
 int camera_port = 0;
+
+int scaleFactor = 4;
+
+cv::Size size(160*scaleFactor, 120*scaleFactor);
 
 bool compareContours(vector<cv::Point> a, vector<cv::Point> b) {
 	return cv::contourArea(a, false) < cv::contourArea(b, false);
 }
 
+cv::Point2f findContourCenter(vector<cv::Point> contour) {
+	cv::Moments mu = cv::moments( contour, false ); 
+	return cv::Point2f( mu.m10/mu.m00 , mu.m01/mu.m00 );
+}
+
 void process(cv::Mat& source0, vector<vector<cv::Point>>& contours) {
-	cv::Size size(320, 240);
 	cv::Size blur(9, 9);
 	//cv::resize(source0, source0, size);
 	cv::blur(source0, source0, blur);
@@ -27,23 +41,39 @@ void process(cv::Mat& source0, vector<vector<cv::Point>>& contours) {
 	cv::findContours( source0, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
 	
 	sort(contours.begin(), contours.end(), compareContours);
-
 	
 }
 
 int main(int argc, char *argv[]) {
+	cout << "Welcome to WOPR-JR-Vision" << endl;
+	cout<<cv::gpu::getCudaEnabledDeviceCount()<<endl;
 
 	cv::Mat img, toshow;
         vector<vector<cv::Point>> contours;
 	cv::VideoCapture camera(camera_port);
 
+	high_resolution_clock::time_point st, et;
+
 	cv::namedWindow("w", cv::WINDOW_AUTOSIZE);
 	while (true) {
+		st = high_resolution_clock::now();
 		camera >> img;
+		cv::resize(img, img, size);
 		toshow = img.clone();
 		process(img, contours);
 		cv::drawContours(toshow, contours, contours.size()-1, cv::Scalar(255, 0, 0));
 		cv::drawContours(toshow, contours, contours.size()-2, cv::Scalar(255, 0, 0));
+
+
+		cv::Point2f avgPoint = (findContourCenter(contours[contours.size()-1]) + findContourCenter(contours[contours.size()-2]));
+		avgPoint.x /= 2.0;
+		avgPoint.y /= 2.0;
+		cv::circle(toshow, avgPoint, 10, cv::Scalar(0, 0, 255));
+
+		et = high_resolution_clock::now();
+		//printf("\rTime: %f", et-st);
+		cout << pow(10.0, 6) / (duration_cast<microseconds>(et -st).count()) << "\r";
+		fflush(stdout);
 		cv::imshow("w", toshow);
 		cv::waitKey(1);
 		
