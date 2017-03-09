6d09b0b2e55f4796017461f1eebe2232229f1637
LN STEMpunks <cade@lnstempunks.org>
2017-03-08 22:43:51 -0500

    Updated launcher

--- launcher.sh
+++ launcher.sh
@@ -5,8 +5,8 @@
 export VISIONREPO=/home/pi/WOPR-JR-Vision
 
 # Camera 0, and 1
-/usr/bin/env python ${VISIONREPO}/src/grip.py -mjpg 5801 -f ${VISIONREPO}/lab.conf --publish -c 0 &
-/usr/bin/env python ${VISIONREPO}/src/grip.py -f ${VISIONREPO}/lab.conf --publish -c 1 &
+/usr/bin/env python ${VISIONREPO}/src/grip.py -mjpg 5802 -f ${VISIONREPO}/lab.conf --publish -c 0 &
+/usr/bin/env python ${VISIONREPO}/src/grip.py -mjpg 5803 -f ${VISIONREPO}/lab.conf --publish -c 1 &
 
 #Use this for serial connection to arduino UNO
 #/usr/bin/env python ${VISIONREPO}/leds/interface.py --serial /dev/ttyACM0 &
