da1a91b50b8cdd14e7cc1b9970ca65b08c697022
LN STEMpunks <cade@lnstempunks.org>
2017-03-08 21:31:16 -0500

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
 
+FROMDIR="$PWD"
+FOLDER="WOPR-JR-Vision"
+TMPDIR="/tmp/$FOLDER"
+
+mkdir -p $TMPDIR
+
+cd $TMPDIR
+
+cp -Rf $FROMDIR/* ./
+
+rm -rf .git
+rm *.tar*
+
+cd ..
+
+tar cfJ $FOLDER.tar.xz $FOLDER
+
+cd $FROMDIR
+
+cp $TMPDIR/../$FOLDER.tar.xz $FOLDER.tar.xz
+
+TAR="$FOLDER.tar.xz"
+
 TARGET="$1"
 #SOURCES=./src/
 SOURCES="WOPR-JR-Vision.tar.xz"
 
 if [ "$TARGET" == "" ]; then
-    TARGET="pi@raspberrypi.local:~/"
+    TARGET="pi@raspberrypi.local"
 fi
-CMD="scp -r $SOURCES $TARGET ${@:2}"
+HERECMD="scp -r $SOURCES $TARGET:~/ ${@:2}"
+EXECMD="cd ~; rm -rf WOPR-JR-Vision; tar xfv $TAR"
 
-echo "$CMD"
-bash -c "$CMD" || echo "Failed"
+echo "Running here: $HERECMD"
+bash -c "$HERECMD" || echo "Failed"
+
+echo "Running on raspi: $EXECMD"
+ssh $TARGET '$EXECMD' || echo "Failed"
+
+
